37876f37b803050f3c34d38f6a33de5e3f6087cc
insa cvl <insa@localhost.localdomain>
2019-06-25 19:05:50 +0200

    error calculated successfully

--- 2nl.py
+++ 2nl.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 # sigmoid function
 def nonlin(x,deriv=False):
     if(deriv==True):
@@ -21,10 +21,18 @@ X = np.array([ 8656346,
 659997,
 289337,
 10578455])
+transformedOutput = []
+expectedTransformedOutput = []
 
- 
+
 # output expected           
 y = np.array([20743128,10638475,3021499,1064225,12101366])
+
+
+for e in y :
+    expectedTransformedOutput.append((math.log10(e) - 1.0) / 8.0)
+
+print ("expectedTransformedOutput : ", expectedTransformedOutput) 
 
 # seed random numbers to make calculation
 # deterministic (just a good practice)
@@ -52,18 +60,23 @@ for iter in xrange(1):
     # forward propagation
     l0 = X
     l1 = np.dot(l0,syn0)
-
+    for e in l1:
+        transformedOutput.append((math.log10(e) - 1.0) / 8.0)
+    print ("transformedOutput : ", transformedOutput)
     # how much did we miss?
-    l1_error = np.power(y-l1,1)
+   
+    l1_error = list(np.array(transformedOutput) - np.array(expectedTransformedOutput))
 
-    print(l1_error)
+    print("error",l1_error)
 
     # multiply how much we missed by the 
     # slope of the sigmoid at the values in l1
     #l1_delta = l1_error * nonlin(l1,True)
-
+    l1_delta = l1_error * nonlin(l1,True)
+    
     # update weights
     #syn0 += np.dot(l0.T,l1_delta)
+    syn0 += np.dot(l0,l1_delta)
 
 print "Output After Training:"
 print l1
