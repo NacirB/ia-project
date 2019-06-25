94f440630c63c2a585fdb055f7e828988623b450
nbouaziz <nacir.bouazizi@insa-cvl.fr>
2019-06-25 13:27:34 +0200

    2 layers neural network

---
+++ 2nl.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+# sigmoid function
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+    
+# input dataset
+X = np.array([ 8656346,
+7678491,
+7212995,
+7059951,
+2291288,
+1695000,
+435301,
+394505,
+332547,
+232384,
+65586,
+659997,
+289337,
+10578455 ])
+    
+# output dataset            
+y = np.array([0.,0,0,0,0]).T
+
+# seed random numbers to make calculation
+# deterministic (just a good practice)
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((14,5)) - 1
+print(syn0)
+for iter in xrange(10000):
+
+    # forward propagation
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+
+    # how much did we miss?
+    l1_error = y - l1
+
+    # multiply how much we missed by the 
+    # slope of the sigmoid at the values in l1
+    l1_delta = l1_error * nonlin(l1,True)
+
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta)
+
+print "Output After Training:"
+print l1
+
