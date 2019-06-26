ffe8b767eb33eb3dd2adfb86358d722a8b327658
insa cvl <insa@localhost.localdomain>
2019-06-26 16:08:36 +0200

    working IA but i'm not sureeeeeeeee :D to be or not to be

--- 2nl.py
+++ 2nl.py
@@ -2,15 +2,23 @@ import numpy as np
 import math
 from sklearn import preprocessing
 # sigmoid function
-def nonlin(x,deriv=False):
-    if(deriv==True):
-        return x*(1-x)
-    return 1/(1+np.exp(-x))
 
-def ReLU(x):
-    return abs(x) * (x > 0)
-    
+def nn(x, w):
+    """Output function y = x * w"""
+    return np.dot(x,w)
+def loss(y, t):
+    """MSE loss function"""
+    return np.mean((t - y)**2)    
+def gradient(w, x, t):
+    """Gradient function. (Remember that y = nn(x, w) = x * w)"""
+    return 2 * np.dot(x.T ,(nn(x, w) - t))
+def delta_w(w_k, x, t, learning_rate):
+    """Update function delta w"""
+    return -learning_rate * np.mean(gradient(w_k, x, t))
+
 # input dataset
+learning_rate = 0.5
+nb_of_iterations = 10000
 X = np.array([[ 8656346,
 7678491,
 7212995,
@@ -60,35 +68,37 @@ syn0 = np.array([[1,0,0 ,0,0],
 print "weights *************************************** \n",syn0
 
 print "***************************************"
-for iter in xrange(5):
+for iter in xrange(nb_of_iterations):
 
     # forward propagation
     l0 = X
    
-    l1 = np.dot(l0,syn0)
+    l1 = nn(l0,syn0)
     #dimension l1 : (1,5)
     print "output ********************************\n",l1
     print "***************************************"
     # how much did we miss?
-    l1_error = l1 - y
-   
+    l1_error = loss(l1,y)
+    
     print "error *************************************** \n",l1_error
     print "***************************************"
 
     # multiply how much we missed by the 
     # slope of the sigmoid at the values in l1
     #l1_delta = l1_error * nonlin(l1,True)
-    
-    print(nonlin(l1,True))
-    l1_delta = l1_error * nonlin(l1,True)
+    l1_delta = delta_w(syn0, l0, y, learning_rate)
     #dimension l1_delta (1,5)
     print "delta *************************************** \n", l1_delta 
     print "***************************************"     
+    
     # update weights
     #syn0 += np.dot(l0.T,l1_delta)
-    print np.dot(l0.T,l1_delta)
+    
     syn0 += np.dot(l0.T,l1_delta)
+
+    
 
 print "Matrix of weights After Training:"
 print syn0
+print l1
 
